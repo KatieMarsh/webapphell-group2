e4fac1be4e75a6432270b874ad320274ddebcc23
Suphapol Wongsawat <38904912+KatieMarsh@users.noreply.github.com>
2023-11-13 21:35:14 +0700

    added disable/enable room

--- index.js
+++ index.js
@@ -9,9 +9,41 @@
  app.use(express.json());
  app.use(express.urlencoded({ extended: true }));
 
-app.get('/home', function (req, res) {
+app.get('/home', function (_req, res) {
     res.sendFile(path.join(__dirname, 'views/project/Page1.html'));
 });
+app.get('/staff/home', function (_req, res) {
+    res.sendFile(path.join(__dirname, 'views/project/Page2.html'));
+});
+app.post('/staff/home/disableroom', function (req,res) {
+    const {room_id} = req.body;
+    const sql = `UPDATE room SET status = 'disabled' WHERE room_id = ?`;
+    con.query(sql,[room_id] ,function (err,results) {
+        if(err) {
+            console.error(err);
+            res.status(500).send("Server error disable room");
+        }
+        else {
+            res.send("Disable room success")
+        }
+    
+    })
+});
+app.post('/staff/home/enableroom', function (req,res) {
+    const {room_id} = req.body;
+    const sql = `UPDATE room SET status = 'enabled' WHERE room_id = ?`;
+    con.query(sql,[room_id] ,function (err,results) {
+        if(err) {
+            console.error(err);
+            res.status(500).send("Server error enable room");
+        }
+        else {
+            res.send("Enable room success")
+        }
+    
+    })
+});
+
 // ---------- login -----------
 app.post('/login', function (req, res) {
     const { username, password } = req.body;
@@ -94,10 +126,6 @@ app.post('/register', function (req, res) {
             }
         })
     })
-});
-// ===== adroom =====
-app.get('/rooms', function (req, res) {
-    res.sendFile(path.join(__dirname, 'views/project/addroom.html'));
 });
 
 // ---------- My Booking -----------
@@ -242,7 +270,7 @@ app.post("/rooms", function (req, res) {
   });
   
   // ------------- Update a room --------------
-  app.put("/rooms/:id", function (req, res) {
+app.put("/rooms/:id", function (req, res) {
     const id = req.params.id;
     const updateRoom = req.body;
     const sql = "UPDATE room SET ? WHERE room_id = ?";
@@ -260,7 +288,7 @@ app.post("/rooms", function (req, res) {
   });
   
   
-  app.get('/addroom', function (req, res) {
+app.get('/addroom', function (req, res) {
     res.sendFile(path.join(__dirname, 'views/project/addroom.html'));
 });
 
